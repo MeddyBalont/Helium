d0f238561f8797b7137de19d924f2ff8695ea709
MeddyBalont <Meddy.Balont@hotmail.com>
2024-07-24 15:50:35 +0200

    Initial import

---
+++ index.js
@@ -0,0 +1,22 @@
+// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+
+// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+
+// S'assurer que les liens sont clickables
+
+// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+// const mouseMouve1 = document.querySelector(".mouseMini");
+// const mouseMouve2 = document.querySelector(".mouseMid");
+// const mouseMouve3 = document.querySelector(".mouseBig");
+
+// window.addEventListener("mousemove", (e) => {
+//   mouseMouve1.style.left = e.pageX + "px";
+//   mouseMouve1.style.top = e.pageY + "px";
+
+//   mouseMouve2.style.left = e.pageX + "px";
+//   mouseMouve2.style.top = e.pageY + "px";
+
+//   mouseMouve3.style.left = e.pageX + "px";
+//   mouseMouve3.style.top = e.pageY + "px";
+// });
