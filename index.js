8f0087cb01e72045316461873a8bac5af563080f
MeddyBalont <Meddy.Balont@hotmail.com>
2024-07-24 16:48:06 +0200

    Mouse Effect

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+//Méthode optimisé
+const mouses = document.querySelectorAll(".mouse");
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
+});
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
-
-// S'assurer que les liens sont clickables
-
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
-
-// const mouseMouve1 = document.querySelector(".mouseMini");
-// const mouseMouve2 = document.querySelector(".mouseMid");
-// const mouseMouve3 = document.querySelector(".mouseBig");
-
+//Méthode simple
 // window.addEventListener("mousemove", (e) => {
-//   mouseMouve1.style.left = e.pageX + "px";
-//   mouseMouve1.style.top = e.pageY + "px";
+//   cursor.style.top = e.y + "px";
+//   cursor.style.left = e.x + "px";
 
-//   mouseMouve2.style.left = e.pageX + "px";
-//   mouseMouve2.style.top = e.pageY + "px";
+//   mouse1.style.top = e.y + "px";
+//   mouse1.style.left = e.x + "px";
 
-//   mouseMouve3.style.left = e.pageX + "px";
-//   mouseMouve3.style.top = e.pageY + "px";
+//   mouse2.style.top = e.y + "px";
+//   mouse2.style.left = e.x + "px";
 // });
+
+// console.log();
